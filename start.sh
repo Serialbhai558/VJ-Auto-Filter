a73d5ffbc81a83c1c917b99e82677d4819f722c4
EasyIce <120899174+IamEasyIce@users.noreply.github.com>
2023-01-24 10:20:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/IamEasyIce/Nataliya /Nataliya
+  git clone https://github.com/IamEasyIce/NataliyaH /NataliyaH
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Nataliya
+  git clone $UPSTREAM_REPO /NataliyaH
 fi
-cd /Priyanka
+cd /NataliyaH
 pip3 install -U -r requirements.txt
 echo "Starting Nataliya...."
 python3 bot.py
