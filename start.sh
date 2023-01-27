9d543eae187984a9ff2f4a5e3d1ed842f9c6f57b
EasyIce <120899174+IamEasyIce@users.noreply.github.com>
2023-01-27 08:56:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/IamEasyIce/NataliyaH /NataliyaH
+  git clone https://github.com/IamEasyIce/NataliyaH.git /NataliyaH
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /NataliyaH
